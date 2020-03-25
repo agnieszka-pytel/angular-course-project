4367732156102ad3d2d90352fd905230aae92f2d
agnieszka-pytel <agnieszkapytel25@gmail.com>
2020-03-25 15:19:00 +0100

    Fixed imports after moving components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HeaderComponent } from './layout/header/header.component';
-import { FooterComponent } from './layout/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 import {MatMenuModule} from '@angular/material/menu';
 import {MatIconModule} from '@angular/material/icon';
@@ -26,4 +26,5 @@ import {MatIconModule} from '@angular/material/icon';
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
