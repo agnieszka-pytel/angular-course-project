685b923945977811881381a8914dbe92d93f10b3
agnieszka-pytel <agnieszkapytel25@gmail.com>
2020-03-25 13:14:44 +0100

    Added routing, basic components and history page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HistoryComponent } from './pages/history/history.component';
+import { BookDetailsComponent } from './components/book-details/book-details.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+    {path: '',
+        redirectTo: '/history',
+        pathMatch: 'full'},
+    {path: 'history', component: HistoryComponent},
+    {path: 'book/:id', component: BookDetailsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/components/book-list/book-list.component.ts
@@ -0,0 +1,9 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'app-book-list',
+  templateUrl: './book-list.component.html',
+  styleUrls: ['./book-list.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class BookListComponent {}
