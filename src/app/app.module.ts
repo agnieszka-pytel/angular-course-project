29a0cbb7582162fa45354dfca63c545cb392a48b
Agnieszka Pytel <agnieszkapytel25@gmail.com>
2020-03-27 09:56:41 +0100

    Header footer (#3)
    * Added header and footer
    
    * Moved header and footer to components directory
    
    * Changed menu structure and layout styles
    
    * Changed default scss index file name
    
    * Fixed imports after moving components
    
    * Added empty lines between parts of code
    
    * Removed unnecessary code
    
    * Changed changeDetection strategy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,27 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+import {MatMenuModule} from '@angular/material/menu';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatMenuModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
